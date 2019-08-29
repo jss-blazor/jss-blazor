33187136d7aee22686edcbc46599e34fd8f23a54
Gary Wenneker <gary@gary.nu>
2019-08-29 16:43:03 +0200

    Implement Checkbox component

--- src/JssBlazor.Components/Utilities/LayoutDataUtilities.cs
+++ src/JssBlazor.Components/Utilities/LayoutDataUtilities.cs
@@ -24,11 +24,24 @@ namespace JssBlazor.Components.Utilities
         public static string GetFieldValue(IRendering rendering, string fieldName, string defaultValue = null)
         {
             if (rendering == null) return defaultValue;
+            string renderedValue = null;
 
             var fields = rendering.Fields;
             if (fields == null) return defaultValue;
 
-            var renderedValue = fields[fieldName]?.Value?.Rendered;
+            if(fields[fieldName] != null)
+            {
+                var value = fields[fieldName]?.Value;
+                switch(value.GetType().Name)
+                {
+                    case "BooleanFieldValue":
+                        renderedValue = fields[fieldName]?.Value.RawValue.ToString();
+                        break;
+                    default:
+                        renderedValue = fields[fieldName]?.Value?.Rendered;
+                        break;
+                }
+            }
             return string.IsNullOrWhiteSpace(renderedValue) ? defaultValue : renderedValue;
         }
     }
