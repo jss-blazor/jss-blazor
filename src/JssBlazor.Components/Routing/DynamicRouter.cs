9ac04aa6689e902ea42825a6d29f37914ab35c2d
Corey Smith <coreyas@gmail.com>
2019-10-15 01:55:43 -0400

    Create DynamicRouter so Sitecore routes don't need to be defined with @page attributes

---
+++ src/JssBlazor.Components/Routing/DynamicRouter.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace JssBlazor.Components.Routing
+{
+    public class DynamicRouter : IComponent, IHandleAfterRender, IDisposable
+    {
+        static readonly char[] _queryOrHashStartChar = new[] { '?', '#' };
+
+        static readonly ReadOnlyDictionary<string, object> _emptyParametersDictionary =
+            new ReadOnlyDictionary<string, object>(new Dictionary<string, object>());
+
+        RenderHandle _renderHandle;
+        string _baseUri;
+        string _locationAbsolute;
+        bool _navigationInterceptionEnabled;
+        bool _disposed = false;
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        private INavigationInterception NavigationInterception { get; set; }
+
+        [Parameter]
+        public Type Layout { get; set; }
+
+        /// <summary>
+        /// Gets or sets a collection of additional assemblies that should be searched for components
+        /// that can match URIs.
+        /// </summary>
+        [Parameter]
+        public IEnumerable<Assembly> AdditionalAssemblies { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content to display when no match is found for the requested route.
+        /// </summary>
+        [Parameter]
+        public RenderFragment NotFound { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content to display when a match is found for the requested route.
+        /// </summary>
+        [Parameter]
+        public RenderFragment<RouteData> Found { get; set; }
+
+        /// <inheritdoc />
+        public void Attach(RenderHandle renderHandle)
+        {
+            _renderHandle = renderHandle;
+            _baseUri = NavigationManager.BaseUri;
+            _locationAbsolute = NavigationManager.Uri;
+            NavigationManager.LocationChanged += OnLocationChanged;
+        }
+
+        /// <inheritdoc />
+        public Task SetParametersAsync(ParameterView parameters)
+        {
+            parameters.SetParameterProperties(this);
+
+            if (Layout == null)
+            {
+                throw new InvalidOperationException($"The {nameof(DynamicRouter)} component requires a value for the parameter {nameof(Layout)}.");
+            }
+
+            // Found content is mandatory, because even though we could use something like <RouteView ...> as a
+            // reasonable default, if it's not declared explicitly in the template then people will have no way
+            // to discover how to customize this (e.g., to add authorization).
+            if (Found == null)
+            {
+                throw new InvalidOperationException($"The {nameof(DynamicRouter)} component requires a value for the parameter {nameof(Found)}.");
+            }
+
+            // NotFound content is mandatory, because even though we could display a default message like "Not found",
+            // it has to be specified explicitly so that it can also be wrapped in a specific layout
+            if (NotFound == null)
+            {
+                throw new InvalidOperationException($"The {nameof(DynamicRouter)} component requires a value for the parameter {nameof(NotFound)}.");
+            }
+
+            Refresh();
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                NavigationManager.LocationChanged -= OnLocationChanged;
+            }
+
+            _disposed = true;
+        }
+
+        private static string StringUntilAny(string str, char[] chars)
+        {
+            var firstIndex = str.IndexOfAny(chars);
+            return firstIndex < 0
+                ? str
+                : str.Substring(0, firstIndex);
+        }
+
+        private void Refresh()
+        {
+            var locationPath = NavigationManager.ToBaseRelativePath(_locationAbsolute);
+            locationPath = StringUntilAny(locationPath, _queryOrHashStartChar);
+
+            var routeData = new RouteData(Layout, _emptyParametersDictionary);
+            _renderHandle.Render(Found(routeData));
+        }
+
+        private void OnLocationChanged(object sender, LocationChangedEventArgs args)
+        {
+            _locationAbsolute = args.Location;
+            if (_renderHandle.IsInitialized)
+            {
+                Refresh();
+            }
+        }
+
+        Task IHandleAfterRender.OnAfterRenderAsync()
+        {
+            if (!_navigationInterceptionEnabled)
+            {
+                _navigationInterceptionEnabled = true;
+                return NavigationInterception.EnableNavigationInterceptionAsync();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
