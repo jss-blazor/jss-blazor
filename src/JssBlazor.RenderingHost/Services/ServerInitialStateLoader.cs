74ec78a25390082abef0f8e9dd311f3dc5603480
Corey Smith <coreyas@gmail.com>
2019-10-28 23:32:39 -0400

    Move ServerInitialStateLoader to JssBlazor.RenderingHost

--- src/JssBlazor.RenderingHost/Services/ServerInitialStateLoader.cs
+++ src/JssBlazor.RenderingHost/Services/ServerInitialStateLoader.cs
@@ -1,13 +1,14 @@
 using System.Threading.Tasks;
 using JssBlazor.Core.Models.LayoutService;
+using JssBlazor.Core.Services;
 
-namespace JssBlazor.Core.Services
+namespace JssBlazor.RenderingHost.Services
 {
     public class ServerInitialStateLoader : IInitialStateLoader
     {
         public Task<LayoutServiceResult> GetInitialStateAsync()
         {
-            return Task.FromResult<LayoutServiceResult>(null); ;
+            return Task.FromResult<LayoutServiceResult>(null);
         }
     }
 }
